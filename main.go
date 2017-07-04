886a925b101422e32972afff71ae2e7f75529849
任 琨 <renkun@renkun.local>
2017-07-05 00:15:58 +0800

    添加运行提示

--- main.go
+++ main.go
@@ -1,3 +1,25 @@
-func main(){
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
 	fmt.Println("hello updatefile project!")
+	args := os.Args
+	if len(args) < 2 {
+		//不带参数，显示说明
+		fmt.Println("这是一个依托 ftp 和 sftp 进行文件更新的程序")
+		fmt.Println("                  Author By BluePrint")
+		fmt.Println("Application  process ... ...")
+	} else {
+		//带参数
+		fmt.Println(args)
+	}
+
+	// h := md5.New()
+	// h.Write([]byte(args[1]))
+	// fmt.Println(h.Sum(nil))
+	// fmt.Println(hex.EncodeToString(h.Sum(nil)))
 }
